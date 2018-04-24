e32fd613236f3f1a7a71d46a36d297bf76f8b346
JuanpiRossi <rossi_juanpablo@hotmail.com>
2018-04-24 20:21:32 -0300

    asd

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { pageList } from '../pages.list';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  htmlOut="";
+  pageList=pageList;
+  constructor(private sanitizer: DomSanitizer) { 
+    this.htmlOut = '<div class="menuComponentContainer"><div class="menuComponent" onclick="window.location=\'/#/roster\';"><div class="componentTitle">Roster</div><div class="componentDescription">Guild roster list</div></div></div><div class="menuComponentContainer"><div class="menuComponent" onclick="window.location=\'/#/parses\';"><div class="componentTitle">Parses Overall</div><div class="componentDescription">Guild parses list overall</div></div></div><div class="menuComponentContainer"><div class="menuComponent" onclick="window.location=\'/#/parsesIlvl\';"><div class="componentTitle">Parses by ilvl</div><div class="componentDescription">Guild parses list by item level</div></div></div>'
+  }
 
   ngOnInit() {
+    // pageList.forEach(element => {
+    //   this.htmlOut = this.htmlOut + '<div class="menuComponentContainer"><div class="menuComponent" style="cursor: pointer;" onclick="window.location=\'/#/roster\';"><div class="componentTitle">'+element.title+'</div><div class="componentDescription">'+element.description+'</div></div></div>';
+    // });
   }
 
 }
---
+++ src/app/core/pages/pages.list.ts
@@ -0,0 +1,18 @@
+export const pageList =[
+    {
+        route:"roster",
+        title:"Roster",
+        description:"Guild roster list"
+    },
+    {
+        route:"parses",
+        title:"Parses Overall",
+        description:"Guild parses list overall"
+    },
+    {
+        route:"parsesIlvl",
+        title:"Parses by ilvl",
+        description:"Guild parses list by item level"
+    },
+];
+  
